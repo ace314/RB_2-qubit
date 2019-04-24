bf2c7e2e8df13a326aad515beb980658c0fd767e
chiang che <wefackinlost314@gmail.com>
2019-04-24 13:05:22 +0800

    Generating L=3 Ciff_2 decomposition.
    Generating 2 qubit Cifford group of which elements decomposed by
    primitive and virtual-Z gates. Here we only iterate to  L=3, with total
    16+384+4176+6912=11488 Clifford elements (other 32 elements should be
    constructed with L=4).
    The result is saved in "Cliff_indices.pkl" with index convertion
    mentioned in "index_conv.txt".

--- C_2 decompose.py
+++ C_2 decompose.py
@@ -1,68 +1,10 @@
 import numpy as np
-# import qecc as q
 # import copy
 import itertools
+import pickle
 
-# def Generate_symp_basis(n):
-#     P = np.zeros(shape=(2*n, 2**n, 2**n), dtype=complex)
-#     for i in range(n):  #P裡的第i及i+n個矩陣
-#         for j in range(n):  #第i / i+n個矩陣的第j個kron為X / Z
-#             if j == 0:
-#                 if j == i:
-#                     a = X
-#                     b = Z
-#                 else:
-#                     a = I
-#                     b = I
-#             else:
-#                 if j == i:
-#                     a = np.kron(a, X)
-#                     b = np.kron(b, Z)
-#                 else:
-#                     a = np.kron(a, I)
-#                     b = np.kron(b, I)
-#         P[i] = copy.deepcopy(a)
-#         P[i+n] = copy.deepcopy(b)
-#     return P
-#
-# def Is_pauli(M):
-#     for i in range(len(P_n)):
-#         if np.allclose(M, P_n[i], rtol=1e-05, atol=1e-08, equal_nan=False):
-#             return True
-#         else:
-#             continue
-#     return False
-#
-# def Is_clifford(M):
-#     for i in range(len(Sym_set)):
-#         a = Sym_set[i]
-#         N = a.dot(M).dot(a.conj().T)
-#         return Is_pauli(N)
-
-#Generate a set [I, X, Y, Z]
-
-#Generate n-qubit pauli group [I, X, Y, Z]
-# def Generate_pauli(n):
-#     a = q.pauli_group(n)
-#     next(a)
-#     P = []
-#     for i in range(4**n-1):
-#         b = copy.deepcopy(next(a)).as_unitary()
-#         P.append(b)
-#         P.append(-b)
-#     # return P
-
-#swap M(q1, q2) to M(q2, q1). For 4x4 matrix only.
-# def Pauli_mat():
-#     a = q.pauli_group(1)
-#     P = []
-#     for i in range(4):
-#         b = copy.deepcopy(next(a)).as_unitary()
-#         P.append(b)
-#     return P
 
 def swap(M):
-    # SWAP = q.swap(2, 0, 1).as_unitary()
     SWAP = np.array([[1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1]])
     return np.dot(np.dot(SWAP, M), SWAP)
 
@@ -87,10 +29,7 @@ def Is_New(M_array, M):
             continue
     return True
 
-# global P_n
-# P_n = Generate_pauli(2)
-# global Pauli
-# Pauli = Pauli_mat()
+
 global I
 global X
 global Y
@@ -99,8 +38,7 @@ I = np.identity(2)
 X = np.array([[0,   1], [1,  0]])
 Y = np.array([[0, -1j], [1j, 0]])
 Z = np.array([[1,   0], [0, -1]])
-# global Sym_set
-# Sym_set = Generate_symp_basis(2)
+
 
 Z_2 = (1/np.sqrt(2))*np.array([[1+1j, 0+0j], [0+0j, 1-1j]])
 CNOT = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0]])
@@ -119,41 +57,46 @@ Zv = [Zv_2]
 for i in range(1, 4):
     Zv.append(np.dot(Zv[i-1], Zv_2))
 
-Zv_q1 = [ [[['Zv(pi/2)', 1]]      ],
-          [[['Zv(pi)', 1]]        ],
-          [[['Zv(3pi/2)', 1]]     ],
-          [[['Zv(2pi)', 1]]       ] ]
+Zv_q1 = [ [[8]      ],                  #['Zv(pi/2)', 1]        = 8
+          [[9]      ],                  #['Zv(pi)', 1]          = 9
+          [[10]     ],                  #['Zv(3pi/2)', 1]       = 10
+          [[]       ] ]                 #['Zv(2pi)', 1]         = identity = none
 
-Zv_q2 = [ [[['Zv(pi/2)', 2]]      ],
-          [[['Zv(pi)', 2]]        ],
-          [[['Zv(3pi/2)', 2]]     ],
-          [[['Zv(2pi)', 2]]       ] ]
+Zv_q2 = [ [[11]     ],                  #['Zv(pi/2)', 2]        = 11
+          [[12]     ],                  #['Zv(pi)', 2]          = 12
+          [[13]     ],                  #['Zv(3pi/2)', 2]       = 13
+          [[]       ] ]                 #['Zv(2pi)', 2]         = identity = none
 
 for i in range(4):
     Zv_q1[i].append(np.kron(Zv[i], I))
     Zv_q2[i].append(np.kron(I, Zv[i]))
 
 #generate all possible primitive gates set
-Prim = [ [[['X(pi/2)', 2]], np.kron(I, X_2)], [[['X(pi/2)', 1]], swap(np.kron(I, X_2))],
-         [[['X(pi/2)+CROT', 2]], X_CROT], [[['X(pi/2)+CROT', 1]], swap(X_CROT)],
-         [[['Z(pi/2)+CROT', 2]], Z_CROT], [[['Z(pi/2)+CROT', 1]], swap(Z_CROT)],
-         [[['CROT', 2]], CROT], [[['CROT', 1]], swap(CROT)] ]
+Prim = [ [[0], np.kron(I, X_2)],        #['X(pi/2)', 2]         = 0
+         [[1], swap(np.kron(I, X_2))],  #['X(pi/2)', 1]         = 1
+         [[2], X_CROT],                 #['X(pi/2)+CROT', 2]    = 2
+         [[3], swap(X_CROT)],           #['X(pi/2)+CROT', 1]    = 3
+         [[4], Z_CROT],                 #['Z(pi/2)+CROT', 2]    = 4
+         [[5], swap(Z_CROT)],           #['Z(pi/2)+CROT', 1]    = 5
+         [[6], CROT],                   #['CROT', 2]            = 6
+         [[7], swap(CROT)] ]            #['CROT', 1]            = 7
 
-#generate set contains all possible primitive gates with following Zv(4 possibilities each). 8*4=32 elements.
+#generate set contains all possible primitive gates with following Zv in the circuit(4 possibilities each). 8*4=32 elements.
 Prim_Zv = []
 for i in range(8):
     if i%2==0:
         for j in range(4):
-            Prim_Zv.append(Gates_multi(Prim[i], Zv_q2[j]))
+            Prim_Zv.append(Gates_multi(Zv_q2[j], Prim[i]))
     else:
         for j in range(4):
-            Prim_Zv.append(Gates_multi(Prim[i], Zv_q1[j]))
+            Prim_Zv.append(Gates_multi(Zv_q1[j], Prim[i]))
 
 '''
 Computational searching all possible clifford elements.
 '''
 
 Cliff_2 = []
+Cliff_index = []
 L = []
 c = 0
 #Iterate first Zv's
@@ -161,36 +104,39 @@ for i, j in itertools.product(range(4), range(4)):
     a = Gates_multi(Zv_q1[i], Zv_q2[j])
     if Is_New(Cliff_2, a):
         Cliff_2.append(a)
+        Cliff_index.append(a[0])
         c += 1
     else:
         continue
 L.append(c)
 c = 0
 
-l = 1
+l = 3
+t = 1
 d = 0
-while l<=1:
-    if l>1:
-        d += L[l-2]
+while t<=l:
+    if t>1:
+        d += L[t-2]
     for i in range(d, len(Cliff_2)):
         for j in range(len(Prim_Zv)):
-            a = Gates_multi(Cliff_2[i], Prim_Zv[j])
+            a = Gates_multi(Prim_Zv[j], Cliff_2[i])
             if Is_New(Cliff_2, a):
                 Cliff_2.append(a)
+                Cliff_index.append(a[0])
                 c += 1
             else:
                 continue
     L.append(c)
     c = 0
-    l += 1
+    t += 1
+
+with open('Cliff_indices.pkl', 'wb') as f:
+    pickle.dump(Cliff_index, f)
 
 print(L, "\n")
-# print(np.absolute(np.trace(np.dot(Cliff_2[16][1].conj().T, Cliff_2[528][1]))), "\n")
-# print(np.allclose(np.absolute(np.trace(np.dot(Cliff_2[16][1].conj().T, Cliff_2[528][1]))), 4), "\n")
-# print(Is_New(Cliff_2, Cliff_2[528]), "\n")
-# print(np.absolute(np.trace(np.dot(Cliff_2[20][1].conj().T, 1j*Cliff_2[20][1]))), "\n")
 # print(Cliff_2[199][0], "\n\n", Cliff_2[199][1])
-# print(Cliff_2[16][1], "\n\n", Cliff_2[528][1])
+# print(Cliff_index)
+
 
 
 
